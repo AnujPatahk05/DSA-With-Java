915528c2a16b46bca502a7e363f04b179d3d6bcf
Anuj Patahk <anujpathakanuj371@gmail.com>
2026-04-16 18:15:12 +0530

    Revising LinkedList

--- Revesion.java
+++ Revesion.java
@@ -1,18 +1,101 @@
-import java.util.ArrayList;
+
+class LinkedList{
+    private class Node{
+        int data;
+        Node next;
+
+        Node(int data){
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    private  Node head;
+    private Node tail;
+    private int size;
+
+    public void addFirst(int data){
+        Node newNode = new Node(data);
+        size++;
+
+        if(head == null){
+            head = tail = newNode;
+            return;
+        }
+
+        newNode.next = head;
+        head = newNode;
+    }
+
+    public void addLast(int data){
+        Node newNode = new Node(data);
+        size++;
+
+        if(tail == null){
+            head = tail = newNode;
+        }
+
+        tail.next = newNode;
+        tail = newNode;
+    }
+
+    public void add(int data,int index){
+        if(index > size){
+            System.out.println("Index out of bound");
+            return;
+        }
+
+        if(index == 0){
+            addFirst(data);
+            return;
+        }else if(index == size){
+            addLast(data);
+            return;
+        }
+
+        Node prev = head;
+
+        for(int i = 0;i < index-1;i++){
+            prev = prev.next;
+        }
+
+        Node newNode = new Node(data);
+
+        newNode.next = prev.next;
+        prev.next = newNode;
+
+        size++;
+    }
+
+    public void printList(){
+        Node node = head;
+        while(node != null){
+            System.out.print(node.data+" ");
+            node = node.next;
+        }
+        System.out.println();
+    }
+
+    public int size(){
+        return size;
+    }
+}
 
 public class Revesion{
-    public static void permutation(String str){
-        
-    }
-
-    public static ArrayList<Integer> digits (int n){
-
-        return new ArrayList<>();
-    }
-    
     public static  void main(String[] args) {
-        int n = Integer.parseInt("0123");
-        String s = String.valueOf(n);
-        System.out.println(n);
+        LinkedList ll = new LinkedList();
+        ll.addLast(1);
+        ll.addLast(2);
+        ll.addLast(3);
+        ll.addLast(4);
+        ll.addLast(5);
+        ll.addLast(6);
+
+        ll.printList();
+
+        ll.add(22, 6);
+
+        ll.printList();
+        // System.out.println(ll.size());
     }
 }
