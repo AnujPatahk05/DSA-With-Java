6072441da3ad4e9bed8ff094fcf82986ae591b50
Anuj Patahk <anujpathakanuj371@gmail.com>
2026-06-28 22:23:21 +0530

    Solve Can Place Flowers problem using greedy strategy

---
+++ Leetcode Practice/1. Array/Other/CanPlaceFlowers.java
@@ -0,0 +1,52 @@
+/*
+    605. Can Place Flowers
+    (easy)
+
+    You have a long flowerbed in which some of the plots are planted, and some are not. However, 
+    flowers cannot be planted in adjacent plots.
+
+    Given an integer array flowerbed containing 0's and 1's, where 0 means empty and 1 means not empty, 
+    and an integer n, return true if n new flowers can be planted in the flowerbed without violating the 
+    no-adjacent-flowers rule and false otherwise.
+
+    Example 1:
+    Input: flowerbed = [1,0,0,0,1], n = 1
+    Output: true
+
+    Example 2:
+    Input: flowerbed = [1,0,0,0,1], n = 2
+    Output: false
+*/
+
+public class CanPlaceFlowers {
+    // TC: O(n)
+    // SC: O(1)
+    public static boolean canPlaceFlowers(int[] flowerbed, int n) {
+        if(n == 0) return true;
+
+        for(int i = 0;i < flowerbed.length;i++) {
+            if(flowerbed[i] == 1) continue;
+
+            int left = (i == 0) ? 0 : flowerbed[i - 1];
+            int right = (i == flowerbed.length - 1) ? 0 : flowerbed[i + 1];
+
+            if(left == 0 && right == 0) {
+                flowerbed[i] = 1;
+                n--;
+            }
+
+            if(n == 0) return true;
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int[] flowerbed1 = {1,0,0,0,1};
+        int n1 = 1;
+        System.out.println(canPlaceFlowers(flowerbed1, n1));
+
+        int[] flowerbed2 = {1,0,0,0,1};
+        int n2 = 2;
+        System.out.println(canPlaceFlowers(flowerbed2, n2));
+    }
+}
