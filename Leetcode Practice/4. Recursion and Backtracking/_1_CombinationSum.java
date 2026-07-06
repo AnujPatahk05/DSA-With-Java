5cab96398f1cd2827d1753d87b0fc4a235a4dbc6
Anuj Patahk <anujpathakanuj371@gmail.com>
2026-07-06 12:13:32 +0530

    Solve Combination Sum problem using backtracking

---
+++ Leetcode Practice/4. Recursion and Backtracking/_1_CombinationSum.java
@@ -0,0 +1,76 @@
+/*
+    39. Combination Sum
+    (Medium)
+
+    Given an array of distinct integers candidates and a target integer target, return a list of all unique 
+    combinations of candidates where the chosen numbers sum to target. You may return the combinations in any order.
+
+    The same number may be chosen from candidates an unlimited number of times. Two combinations are unique if the 
+    frequency of at least one of the chosen numbers is different.
+
+    The test cases are generated such that the number of unique combinations that sum up to target is less than 150 
+    combinations for the given input.
+
+    Example 1:
+
+    Input: candidates = [2,3,6,7], target = 7
+    Output: [[2,2,3],[7]]
+
+    Explanation:
+    2 and 3 are candidates, and 2 + 2 + 3 = 7. Note that 2 can be used multiple times.
+    7 is a candidate, and 7 = 7.
+    These are the only two combinations.
+
+    Example 2:
+
+    Input: candidates = [2,3,5], target = 8
+    Output: [[2,2,2,2],[2,3,3],[3,5]]
+*/
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class _1_CombinationSum {
+    // TC: 2 ^ (target/min(candidates)) (exponential)
+    // SC: O(target/min(candidates))
+    private static void combinationSumUtil(int[] candidates, 
+                                           int target,
+                                           int sum,
+                                           int i,
+                                           List<Integer> currResult,
+                                           List<List<Integer>> result) {
+        
+        if (sum == target) {
+            result.add(new ArrayList<>(currResult));
+            return;
+        }
+
+        if(sum > target || i == candidates.length) {
+            return;
+        }
+
+        currResult.add(candidates[i]);
+        // combinationSumUtil(candidates, target, sum+candidates[i], i + 1, currResult, result);
+
+        combinationSumUtil(candidates, target, sum+candidates[i], i, currResult, result);
+        currResult.remove(currResult.size() - 1);
+
+        combinationSumUtil(candidates, target, sum, i+1, currResult, result);
+    }
+
+    public static List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> result = new ArrayList<>();
+        combinationSumUtil(candidates, target, 0, 0, new ArrayList<>(), result);
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] arr1 = {2,3,6,7};
+        int target = 7;
+        System.out.println(combinationSum(arr1, target));        
+
+        int[] arr2 = {2,3,5};
+        int target2 = 8;
+        System.out.println(combinationSum(arr2, target2));
+    }
+}
